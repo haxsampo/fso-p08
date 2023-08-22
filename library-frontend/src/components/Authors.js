d62096ccbf39466c3db7ed1aa7191020d90f7553
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-08-22 12:57:53 +0300

    8.8 ok

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,35 @@
+const Authors = (props) => {
+  //console.log("props.authors",props.authors)
+  if (!props.show) {
+    return null
+  }
+  if (props.authors.loading) {
+    return null
+  }
+  //console.log("props.authors.data",props.authors.data.allAuthors)
+  const authors = props.authors.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
