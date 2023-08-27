7fa041f12e596f90f9aa854f3faa93a8bc304258
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-08-27 18:58:35 +0300

    8.20

--- library-frontend/src/components/Favourites.js
+++ library-frontend/src/components/Favourites.js
@@ -1,7 +1,7 @@
 import {gql, useQuery} from '@apollo/client'
 
 
-const b = `query Me {
+const me = gql`query Me {
   me {
     username
     favoriteGenre
@@ -9,8 +9,38 @@ const b = `query Me {
 }`
 
 const Favourites = (props) => {
+  const token = localStorage.getItem('user-token')
+  const {loading, error, data} = useQuery(me)
+
+  if(loading) {
+    return null
+  }
+
+  if (!loading && data) {
+    console.log("me data:",data.me.favoriteGenre)
+  }
+
+  console.log("props books", props.allbooks.data.allBooks)
+  const books = props.allbooks.data.allBooks
+  const fbooks = books.filter((b) => b.genres.includes(data.me.favoriteGenre))
+  console.log(fbooks)
   return(
-    <p>Favourites</p>
+    <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {fbooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
   )
 }
 
